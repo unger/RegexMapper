ca638eb888f796587b4c9ee3592dbb5783c0c36c
Magnus Unger <magnus.unger@stendahls.se>
2015-06-12 11:55:20 +0200

    Added support to manually supply groupnames, to map each group to specific object property name

--- RegexMapper.Tests/RegexMapTests.cs
+++ RegexMapper.Tests/RegexMapTests.cs
@@ -10,13 +10,30 @@
     public class RegexMapTests
     {
         [Test]
-        public void Test()
+        public void TestMapping_WithGroupNamesDefinedInRegex()
         {
             var mapper = new RegexMap();
 
             var result = mapper.Matches<TestModel>(
                             new Regex(@"{Id:(?<Id>\d*),Name:""(?<Name>[^""]*)""}"),
                             @"{Id:1,Name:""Test1""},{Id:12,Name:""Test12""},{Id:123,Name:""Test123""},{Id:1234,Name:""Test1234""}");
+
+            Assert.AreEqual(4, result.Count);
+            Assert.AreEqual(1, result[0].Id);
+            Assert.AreEqual("Test1", result[0].Name);
+            Assert.AreEqual(1234, result[3].Id);
+            Assert.AreEqual("Test1234", result[3].Name);
+        }
+
+        [Test]
+        public void TestMapping_WithGroupNamesDefinedInFunction()
+        {
+            var mapper = new RegexMap();
+
+            var result = mapper.Matches<TestModel>(
+                            new Regex(@"{Id:(\d*),Name:""([^""]*)""}"),
+                            @"{Id:1,Name:""Test1""},{Id:12,Name:""Test12""},{Id:123,Name:""Test123""},{Id:1234,Name:""Test1234""}",
+                            new[] { "Id", "Name" });
 
             Assert.AreEqual(4, result.Count);
             Assert.AreEqual(1, result[0].Id);
--- RegexMapper/RegexMap.cs
+++ RegexMapper/RegexMap.cs
@@ -25,5 +25,30 @@
 
             return SafeMap.Convert<List<Dictionary<string, string>>, List<T>>(matchList);
         }
+
+        public IList<T> Matches<T>(Regex regex, string input, string[] groupNames)
+        {
+            var matchList = new List<Dictionary<string, string>>();
+
+            foreach (Match match in regex.Matches(input))
+            {
+                var dict = new Dictionary<string, string>();
+
+                for (int i = 0; i < groupNames.Length; i++)
+                {
+                    var groupName = groupNames[i];
+
+                    if ((i + 1) < match.Groups.Count)
+                    {
+                        dict.Add(groupName, match.Groups[i + 1].Value);
+                    }
+                }
+
+                matchList.Add(dict);
+            }
+
+
+            return SafeMap.Convert<List<Dictionary<string, string>>, List<T>>(matchList);
+        }
     }
 }
