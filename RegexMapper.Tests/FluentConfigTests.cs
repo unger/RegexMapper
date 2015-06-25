1f7de40cecaebe36b873c0686a5b8f46c5c02247
Magnus Unger <magnus_unger@hotmail.com>
2015-06-25 23:36:12 +0200

    Added fluent configuration

---
+++ RegexMapper.Tests/FluentConfigTests.cs
@@ -0,0 +1,49 @@
+﻿namespace RegexMapper.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class FluentConfigTests
+    {
+        [Test]
+        public void BuildConfiguration_None()
+        {
+            var config = new FluentConfig().BuildConfiguration();
+
+            Assert.True(config.GlobalStringOperation == StringOperation.None);
+        }
+
+        [Test]
+        public void BuildConfiguration_Trim()
+        {
+            var config = new FluentConfig().Trim().BuildConfiguration();
+
+            Assert.True(config.GlobalStringOperation == StringOperation.Trim);
+        }
+
+        [Test]
+        public void BuildConfiguration_UpperCaseFirst()
+        {
+            var config = new FluentConfig().UpperCaseFirst().BuildConfiguration();
+
+            Assert.True(config.GlobalStringOperation == StringOperation.UpperCaseFirst);
+        }
+
+        [Test]
+        public void BuildConfiguration_HtmlDecode()
+        {
+            var config = new FluentConfig().HtmlDecode().BuildConfiguration();
+
+            Assert.True(config.GlobalStringOperation == StringOperation.HtmlDecode);
+        }
+
+        [Test]
+        public void BuildConfiguration_Trim_UpperCaseFirst_HtmlDecode()
+        {
+            var config = new FluentConfig().Trim().UpperCaseFirst().HtmlDecode().BuildConfiguration();
+
+            Assert.True((config.GlobalStringOperation & StringOperation.HtmlDecode) == StringOperation.HtmlDecode);
+        }
+
+    }
+}
---
+++ RegexMapper/FluentConfig.cs
@@ -0,0 +1,35 @@
+﻿namespace RegexMapper
+{
+    public class FluentConfig
+    {
+        private readonly RegexMapConfiguration config;
+
+        public FluentConfig()
+        {
+            this.config = new RegexMapConfiguration();
+        }
+
+        public FluentConfig Trim()
+        {
+            this.config.GlobalStringOperation |= StringOperation.Trim;
+            return this;
+        }
+
+        public FluentConfig UpperCaseFirst()
+        {
+            this.config.GlobalStringOperation |= StringOperation.UpperCaseFirst;
+            return this;
+        }
+
+        public FluentConfig HtmlDecode()
+        {
+            this.config.GlobalStringOperation |= StringOperation.HtmlDecode;
+            return this;
+        }
+
+        public RegexMapConfiguration BuildConfiguration()
+        {
+            return this.config;
+        }
+    }
+}
